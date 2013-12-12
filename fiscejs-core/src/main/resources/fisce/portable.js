fee4ea6b4c48e3adbefa6e736c1ada736eca5154
Cloudee Huang <cloudeecn@gmail.com>
2013-12-12 18:02:26 +0800

    production env.

--- fiscejs-core/src/main/resources/fisce/portable.js
+++ fiscejs-core/src/main/resources/fisce/portable.js
@@ -30,7 +30,7 @@ var FyConfig = {
 	edenSize : 131072,
 	copySize : 32768,
 	stackSize : 16384,
-	debugMode : true,
+	debugMode : false,
 	verboseMode : false,
 	aggresiveGC : false,
 	_ : undefined
